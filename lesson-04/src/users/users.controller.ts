3f980d007caf2b0966ca692f868d363ec6e18a78
Abgr Xenda <github@xendagroup.com>
2025-08-21 01:33:44 +0300

    update user service

--- lesson-04/src/users/users.controller.ts
+++ lesson-04/src/users/users.controller.ts
@@ -6,7 +6,6 @@ import * as usersEntity from './users.entity';
 import { AuthGuard } from '@nestjs/passport';
 
 @Controller('users')
-@UseGuards(AuthGuard('jwt'))
 export class UsersController {
     //======================
     constructor(private userService: UsersService) { }
@@ -17,11 +16,13 @@ export class UsersController {
     // @DELETE
 
     @Get()
-    findAll(@Query('dep') dep?: usersEntity.UserRole,@Query('email') email?: string) {
+    @UseGuards(AuthGuard('jwt'))
+    findAll(@Query('dep') dep?: usersEntity.UserRole, @Query('email') email?: string) {
         return this.userService.findAll(dep, email)
     }
 
     @Get(':id')
+    @UseGuards(AuthGuard('jwt'))
     findOne(@Param('id') id: string) {
         return this.userService.findOne(+id)
     }
@@ -30,18 +31,21 @@ export class UsersController {
     create(@Body() user: usersEntity.Users) {
         return this.userService.create(user)
     }
-  
+
     @Patch(':id')
-    update(@Param('id') id: string, @Body() userUpdate:usersEntity.Users) {
+    @UseGuards(AuthGuard('jwt'))
+    update(@Param('id') id: string, @Body() userUpdate: usersEntity.Users) {
         return this.userService.update(+id, userUpdate)
     }
 
     @Delete()
+    @UseGuards(AuthGuard('jwt'))
     deletedomain(@Query('domain') domain: string) {
         return this.userService.deletedomain(domain)
     }
-        
+
     @Delete(':id')
+    @UseGuards(AuthGuard('jwt'))
     delete(@Param('id') id: string) {
         return this.userService.delete(+id)
     }
